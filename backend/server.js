af10921f5defdb4076565fc176022fb4c6772451
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-05-06 14:31:15 +0500

    Setup Arcjet

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 
 import productRoutes from "./routes/porductRoutes.js";
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
 
 dotenv.config();
 
@@ -16,6 +17,38 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1,
+    });
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "To Many Requests" });
+      } else if (decision.reason.isBot) {
+        res.status(403).json({ error: "Bot access denided" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
+    }
+    if (
+      decision.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed()
+      )
+    ) {
+      res.status(403).json({ error: "Spoofed out detected" });
+      return;
+    }
+    next();
+  } catch (error) {
+    console.log("Arcjet Error", error);
+    next(error);
+  }
+});
+
+app.use("/api/products", productRoutes);
 
 async function initDB() {
   try {
@@ -39,5 +72,3 @@ initDB().then(() => {
     console.log(`Server is running on Port: ${PORT}`);
   });
 });
-
-app.use("/api/products", productRoutes);
