18bf52e458b4a6518383dc058db9ac9a75269679
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-05-12 21:23:21 +0500

    Building Product

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,22 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import { useThemeStore } from "./store/useThemeStore";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
+  const { theme } = useThemeStore();
   return (
     <div
       className=" min-h-screen bg-base-200 transition-colors duration-300 "
-      data-theme="forest"
+      data-theme={theme}
     >
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/product/:id" element={<ProductPage />} />
       </Routes>
+      <Toaster />
     </div>
   );
 };
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link, useResolvedPath } from "react-router-dom";
 import { ShoppingBagIcon, ShoppingCartIcon } from "lucide-react";
+import ThemeSelector from "./ThemeSelector";
+import { useThemeStore } from "../store/useThemeStore";
 
 const Navbar = () => {
   const { pathname } = useResolvedPath();
   const isHomePage = pathname === "/";
+  const { theme, setTheme } = useThemeStore();
   return (
     <div className=" bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50">
       <div className=" max-w-7xl mx-auto">
@@ -14,13 +17,13 @@ const Navbar = () => {
               <div className=" flex items-center gap-2">
                 <ShoppingCartIcon className=" size-9 text-primary" />
                 <span className=" font-semibold font-mono tracking-widest text-2xl bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary">
-                  Khalid Store
+                  KHALID Store
                 </span>
               </div>
             </Link>
           </div>
           <div className="flex items-center gap-4">
-            <p>ThemeSelector</p>
+            <ThemeSelector />
 
             {isHomePage && (
               <div className="indicator">
